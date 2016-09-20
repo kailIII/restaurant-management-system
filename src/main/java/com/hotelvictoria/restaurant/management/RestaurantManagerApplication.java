61b6ebde3b770aacf5f1a1b7045713bc02efa41b
Thai Phan <thai@outlook.com>
2016-09-20 20:54:39 +1000

    Add initial Spring Initialisr files

---
+++ src/main/java/com/hotelvictoria/restaurant/management/RestaurantManagerApplication.java
@@ -0,0 +1,12 @@
+package com.hotelvictoria.restaurant.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagerApplication.class, args);
+	}
+}
