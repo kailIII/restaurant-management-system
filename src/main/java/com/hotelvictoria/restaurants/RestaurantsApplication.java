e4ef177a47a3e668a21e8722a88a9c351fbf2f31
Thai Phan <thai@outlook.com>
2016-09-24 21:11:33 +1000

    Add way to seed data automatically

--- src/main/java/com/hotelvictoria/restaurants/RestaurantsApplication.java
+++ src/main/java/com/hotelvictoria/restaurants/RestaurantsApplication.java
@@ -1,12 +1,41 @@
 package com.hotelvictoria.restaurants;
 
+import com.hotelvictoria.restaurants.models.Manager;
+import com.hotelvictoria.restaurants.models.Restaurant;
+import com.hotelvictoria.restaurants.models.Table;
+import com.hotelvictoria.restaurants.models.Waiter;
+import com.hotelvictoria.restaurants.repositories.ManagerRepository;
+import com.hotelvictoria.restaurants.repositories.RestaurantRepository;
+import com.hotelvictoria.restaurants.repositories.TableRepository;
+import com.hotelvictoria.restaurants.repositories.WaiterRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestaurantsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantsApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner seedDatabase(ManagerRepository managerRepository, RestaurantRepository restaurantRepository, WaiterRepository waiterRepository, TableRepository tableRepository) {
+		return (args) -> {
+			managerRepository.save(new Manager("Nigel Victoria"));
+
+			for (String restaurantName : new String[]{"McDonald's", "KFC"}) {
+				Restaurant restaurant = restaurantRepository.save(new Restaurant(restaurantName));
+				for (int i = 1; i <= 20; i++) {
+					tableRepository.save(new Table("Table " + i, restaurant));
+				}
+			}
+
+			for (String name : new String[]{"John", "Taylor", "Fred"}) {
+				for (String surname : new String[]{"Smith", "Black", "Brown"}) {
+					waiterRepository.save(new Waiter(name + ' ' + surname));
+				}
+			}
+		};
 	}
 }
